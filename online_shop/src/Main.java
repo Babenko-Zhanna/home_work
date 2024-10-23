090ec190c44c73c092119dfeaf626b8474934e9b
Zhanna Babenko <babenko.zh@gmail.com>
2024-10-23 05:24:28 +0300

    extend productsList

--- online_shop/src/Main.java
+++ online_shop/src/Main.java
@@ -4,12 +4,19 @@ public class Main {
     public static void main(String[] args) {
 
         ProductsList productsList = new ProductsList();
+
         productsList.addProduct(new Product(1, "Bounty", 0.75));
+
         productsList.addProductList(List.of(
                 new Product(2, "Salami", 1.99),
-                new Product(3, "Mascarpone", 2.50)
+                new Product(3, "Mascarpone", 2.50),
+                new Product(4, "Raspberry", 2.99),
+                new Product(5, "Pizza", 3.75),
+                new Product(6, "Milk 1L", 1.09),
+                new Product(7, "Wholewheat bread", 2.55),
+                new Product(8, "Salmon 200g", 4.99)
         ));
-        productsList.addProduct(new Product(4, "Raspberry", 2.99));
+
 
         System.out.println("Добрый день! Вы можете заказать следующие товары:");
         System.out.println(productsList);
