4e33084e5ec47262e0bd7d12483d6651cadb5880
Zhanna Babenko <babenko.zh@gmail.com>
2024-10-23 05:56:49 +0300

    final ProductsList

--- online_shop/src/ProductsList.java
+++ online_shop/src/ProductsList.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ProductsList {
     private Scanner scanner = new Scanner(System.in);
     private List<Product> productList;
-    private  List<Product> productsInBasket;
+    private List<Product> productsInBasket;
 
     /*
     public ProductsList(List<Product> productList) {
@@ -20,52 +20,50 @@ public class ProductsList {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < productList.size(); i++) {
-            sb.append(productList.get(i)).append(System.lineSeparator());
+        for (Product product : productList) {
+            sb.append(product).append(System.lineSeparator());
         }
         return sb.toString();
     }
 
-    public List<Product> addProduct(Product product) {
+    public void addProduct(Product product) {
         productList.add(product);
-        return productList;
     }
 
-    public List<Product> addProductList(List<Product> list) {
-        for (int i = 0; i < list.size(); i++) {
-            productList.add(list.get(i));
+    public void addProductList(List<Product> list) {
+        for (Product product : list) {
+            productList.add(product);
         }
-        return productList;
     }
 
-    public List<Product> addProductToBasket () {
+    public List<Product> addProductToBasket() {
         productsInBasket = new ArrayList<>();
 
         while (true) {
             System.out.println("Введите номер товара для добавления в корзину (0 для завершения):");
             int number = scanner.nextInt();
-            if (number==0 || number < 0 || number > productList.size()) {
+            if (number == 0 || number < 0 || number > productList.size()) {
                 return productsInBasket;
             }
-            productsInBasket.add(productList.get(number-1));
+            productsInBasket.add(productList.get(number - 1));
         }
     }
 
-    private double sumPrice () {
+    private double sumPrice() {
         double result = 0;
-        for (int i = 0; i < productsInBasket.size(); i++) {
-            result += productsInBasket.get(i).getPrice();
+        for (Product product : productsInBasket) {
+            result += product.getPrice();
         }
         return result;
     }
 
-    public void printBasket () {
+    public void printBasket() {
         addProductToBasket();
 
         StringBuilder sb = new StringBuilder();
         sb.append("Ваш список покупок: ").append(System.lineSeparator());
-        for (int i = 0; i < productsInBasket.size(); i++) {
-            sb.append(productsInBasket.get(i).shortString()).append(System.lineSeparator());
+        for (Product product : productsInBasket) {
+            sb.append(product.shortString()).append(System.lineSeparator());
         }
         sb.append("Итоговая сумма: ").append(sumPrice()).append(" €").append(System.lineSeparator());
 
@@ -74,19 +72,28 @@ public class ProductsList {
             System.out.println("Ваша корзина пуста");
             System.out.println("1 - продолжить покупки");
             System.out.println("0 - завершить программу");
-            int number = scanner.nextInt();
-            switch (number) {
-                case 1:
-                    printBasket();
-                    break;
-                case 0:
-                    System.out.println("До новых встреч!");
-                    break;
-                default:
-                    System.out.println("Введите 1 или 0");
+
+            boolean cycle = true;
+
+            while (cycle) {
+                int number = scanner.nextInt();
+                switch (number) {
+                    case 1:
+                        printBasket();
+                        cycle = false;
+                        break;
+                    case 0:
+                        System.out.println("До новых встреч!");
+                        cycle = false;
+                        break;
+                    default:
+                        System.out.println("Введите 1 или 0");
+                        break;
+                }
             }
+
         } else {
-            System.out.println(sb.toString());
+            System.out.println(sb);
         }
 
     }
