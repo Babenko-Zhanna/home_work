a6f40c451410efe278d83bc5116fb638cd3d07cc
Zhanna Babenko <babenko.zh@gmail.com>
2024-10-31 03:13:56 +0200

    remove names

--- hw_l35/task2_remove_names/src/Main.java
+++ hw_l35/task2_remove_names/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> names = new ArrayList<>(List.of("Jack", "John", "Slava", "Nick", "Lena", "Mark"));
+        System.out.println(names);
+        removeNames(names, 3, 4);
+        System.out.println(names);
+
+    }
+
+    public static void removeNames (List<String> list, int n, int i) {
+        ListIterator<String> listIterator = list.listIterator(i);
+        int counter = 0;
+        while (counter != n && listIterator.hasNext()){
+            list.remove(i);
+            counter++;
+        }
     }
 }
